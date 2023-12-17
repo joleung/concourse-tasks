71f9327a5d1585ef99003dfb8eaaa2b7b195739d
Jonathan Leung <kaitai@gmail.com>
2023-12-17 21:19:22 +0000

    Add staging ACME server

--- tasks/certbot-getcert/task.sh
+++ tasks/certbot-getcert/task.sh
@@ -1,11 +1,18 @@
 #!//bin/sh
+ACME_SERVER=acme-v02.api.letsencrypt.org
+if [ -z "${STAGING}" ]; then
+  ACME_SERVER="acme-staging-v02.api.letsencrypt.org"
+  echo "Using staging server ${ACME_SERVER};
+fi
+
 echo "${CLOUDFLARE_INI}" > cloudflare.ini;
 chmod 600 cloudflare.ini;
+
 certbot certonly -n --dns-cloudflare \
 --agree-tos --email ${CERTBOT_EMAIL} \
 --dns-cloudflare-credentials cloudflare.ini \
 --dns-cloudflare-propagation-seconds 30 \
---server https://acme-staging-v02.api.letsencrypt.org/directory \
+--server https://${ACME_SERVER}/directory \
 --key-type rsa \
 -d "${CERT_DOMAIN}";
 cp -RL /etc/letsencrypt/live/${CERT_DOMAIN}/* certoutput;
