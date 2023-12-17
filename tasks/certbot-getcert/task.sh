5a95d418fb4fd6162f12b15f3a8e1bf50115f90c
Jonathan Leung <kaitai@gmail.com>
2023-12-17 19:52:37 +0000

    Add certbot-email address field

--- tasks/certbot-getcert/task.sh
+++ tasks/certbot-getcert/task.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 set -eux
 echo "${CLOUDFLARE_INI}" > cloudflare.ini;
-cat cloudflare.ini;
+chmod 600 cloudflare.ini;
+certbot certonly -n --dns-cloudflare \
+--agree-tos --email ${CERTBOT_EMAIL} \
+--dns-cloudflare-credentials cloudflare.ini \
+--dns-cloudflare-propagation-seconds 30 \
+--server https://acme-staging-v02.api.letsencrypt.org/directory \
+--key-type rsa \
+-d "${CERT_DOMAIN}";
+cp -RL /etc/letsencrypt/live/${CERT_DOMAIN}/* certoutput;
+
