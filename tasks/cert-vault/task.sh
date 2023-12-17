70b5c61129893e95a92c550b3dd66c07536cca49
Jonathan Leung <kaitai@gmail.com>
2023-12-17 22:17:03 +0000

    Add chain and fullchain.pem

--- tasks/cert-vault/task.sh
+++ tasks/cert-vault/task.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-vault kv put ${VAULT_PATH}/${CERT_DOMAIN} cert=@certoutput/fullchain.pem key=@certoutput/privkey.pem
+vault kv put ${VAULT_PATH}/${CERT_DOMAIN} cert=@certoutput/cert.pem chain=@certoutput/chain.pem fullchain=@certoutput/fullchain.pem key=@certoutput/privkey.pem
