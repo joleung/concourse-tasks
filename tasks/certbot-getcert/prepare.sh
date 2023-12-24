1e6dd16a064b335ba71d77ec7fa519f6e80bc305
Jonathan Leung <kaitai@gmail.com>
2023-12-24 22:35:00 +0000

    Remove double quote

--- tasks/certbot-getcert/prepare.sh
+++ tasks/certbot-getcert/prepare.sh
@@ -4,9 +4,9 @@ IFS=', ' read -r -a domain_name <<< "${CERT_DOMAINS}"
 domain_cert_param=""
 for d0 in ${domain_name[@]}
 do
-  dp=" -d \"${d0}\" "
+  dp=" -d ${d0} "
   if "${WILDCARD_CERT}"; then
-    dp+=" -d \"*.${d0}\" "
+    dp+=" -d *.${d0} "
   fi
   domain_cert_param="${domain_cert_param} ${dp}";
 done
