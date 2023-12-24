359ca98b1eeecdf7de04e2a222b58e6e465b48de
Jonathan Leung <kaitai@gmail.com>
2023-12-24 22:36:36 +0000

    Add multiple domain name supports

--- tasks/cert-vault/task.sh
+++ tasks/cert-vault/task.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-vault kv put ${VAULT_PATH}/${CERT_DOMAIN} cert=@certoutput/cert.pem chain=@certoutput/chain.pem fullchain=@certoutput/fullchain.pem key=@certoutput/privkey.pem
+domain_name=`cat domaininfo/firstdomain.txt`
+vault kv put ${VAULT_PATH}/${domain_name} cert=@certoutput/cert.pem chain=@certoutput/chain.pem fullchain=@certoutput/fullchain.pem key=@certoutput/privkey.pem
